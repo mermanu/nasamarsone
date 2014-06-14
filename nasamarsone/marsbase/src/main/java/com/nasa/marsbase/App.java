f2d137f0406b37313c23f7e36e65520f7e4003b1
Manuel Mérida <manuelmerida@Mac-mini-de-Manuel.local>
2014-06-14 02:11:09 +0200

    test to interview

--- nasamarsone/marsbase/src/main/java/com/nasa/marsbase/App.java
+++ nasamarsone/marsbase/src/main/java/com/nasa/marsbase/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        MarsBase mrbase = BaseFactory.getBase("5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM 1 4 E LRMMRMMRMRRM 3 3 E MMRMMRMRRM 1 2 N LMLMLMLMM", BaseType.BASE_ONE);
+        MarsBase mrbase = BaseFactory.getBase("5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM", BaseType.BASE_ONE);
         String output = mrbase.execute();
         System.out.println("MarsBase Output to NASA: " + output);
     }
