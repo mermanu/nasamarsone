72cba42f6acfd5a0ca35390b39955b9dc01580fc
Manuel Mérida <manuelmerida@Mac-mini-de-Manuel.local>
2014-06-15 21:15:02 +0200

    pom

--- nasamarsone/assembly/shellscripts/NasaBash.sh
+++ nasamarsone/assembly/shellscripts/NasaBash.sh
@@ -2,11 +2,11 @@
 echo "NASA transmisor beginning to run..."
 
 echo "MarsBase beginning to run..."
-exec "$JAVA_HOME"/bin/java -jar marsbase-${project.version}.jar 'start' &
+exec "$JAVA_HOME"/bin/java -jar marsbase-1.20.jar 'start' &
 echo "MarsBase ON!!"
 
 echo "MarsRover beginning to run..."
-exec "$JAVA_HOME"/bin/java -jar marsrovers-${project.version}.jar 'start' &
+exec "$JAVA_HOME"/bin/java -jar marsrovers-1.20.jar 'start' &
 echo "MarsRovers ON!!"
 
 echo "NASA transmisor ON!!"
