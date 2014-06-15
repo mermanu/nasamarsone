63957af5fa12f0c25d2d6f47888af335d5dad507
Manuel Mérida <manuelmerida@Mac-mini-de-Manuel.local>
2014-06-15 13:25:55 +0200

    new structure

--- nasamarsone/assembly/shellscripts/NasaBash.sh
+++ nasamarsone/assembly/shellscripts/NasaBash.sh
@@ -2,11 +2,11 @@
 echo "NASA transmisor beginning to run..."
 
 echo "MarsBase beginning to run..."
-exec "$JAVA_HOME"/bin/java -jar marsbase-1.9-SNAPSHOT.jar 'start' &
+exec "$JAVA_HOME"/bin/java -jar marsbase-1.10-SNAPSHOT.jar 'start' &
 echo "MarsBase ON!!"
 
 echo "MarsRover beginning to run..."
-exec "$JAVA_HOME"/bin/java -jar marsrovers-1.9-SNAPSHOT.jar 'start' &
+exec "$JAVA_HOME"/bin/java -jar marsrovers-1.10-SNAPSHOT.jar 'start' &
 echo "MarsRovers ON!!"
 
 echo "NASA transmisor ON!!"
